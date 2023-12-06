641ae2a92b1e9022a01527454c8d4d1cede78c93
Horus Lugo <horusgoul@gmail.com>
2023-12-06 20:46:10 +0100

    Setup astro-demo

---
+++ apps/astro-demo/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import styleX from "vite-plugin-stylex";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), styleX()],
+});
