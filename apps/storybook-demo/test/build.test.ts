0a9ec7c59691609692ec74d9c1116a72bf097f2b
Horus Lugo <horusgoul@gmail.com>
2023-12-14 05:56:22 +0100

    Storybook e2e tests

--- apps/storybook-demo/test/build.test.ts
+++ apps/storybook-demo/test/build.test.ts
@@ -1,10 +1,10 @@
 import { describe, test, before, after } from "node:test";
-import * as vite from "vite";
 import { VITE_ROOT } from "./utils";
 import * as assert from "node:assert";
 import { makeTempDir } from "@internal/test-utils";
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
+import * as cp from "node:child_process";
 
 describe("build", () => {
   let tempDir: string;
@@ -18,11 +18,14 @@ describe("build", () => {
   });
 
   test("builds without crashing", async () => {
-    const build = await vite.build({
-      root: VITE_ROOT,
-    });
-
-    assert.ok(build, "build should be truthy");
+    const build = cp.spawnSync(
+      "pnpm",
+      ["build-storybook", "--output-dir", tempDir],
+      {
+        cwd: VITE_ROOT,
+      }
+    );
+    assert.equal(build.status, 0, "build should exit with status code 0");
   });
 });
 
@@ -32,11 +35,8 @@ describe("build output", () => {
   before(async () => {
     tempDir = await makeTempDir();
 
-    await vite.build({
-      root: VITE_ROOT,
-      build: {
-        outDir: tempDir,
-      },
+    cp.spawnSync("pnpm", ["build-storybook", "--output-dir", tempDir], {
+      cwd: VITE_ROOT,
     });
   });
 
@@ -53,18 +53,18 @@ describe("build output", () => {
     assert.ok(stylexFile, "an stylex file should exist in the build output");
   });
 
-  test("index.html contains a link to the stylex stylesheet", async () => {
+  test("iframe.html contains a link to the stylex stylesheet", async () => {
     const indexHtml = await fs.readFile(
-      path.join(tempDir, "index.html"),
+      path.join(tempDir, "iframe.html"),
       "utf-8"
     );
     const stylexLink = indexHtml.includes(
-      `<link rel="stylesheet" href="/assets/stylex.`
+      `<link rel="stylesheet" href="assets/stylex.`
     );
 
     assert.ok(
       stylexLink,
-      "index.html should contain a link to the stylex stylesheet"
+      "iframe.html should contain a link to the stylex stylesheet"
     );
   });
 
--- apps/storybook-demo/test/dev.test.ts
+++ apps/storybook-demo/test/dev.test.ts
@@ -1,6 +1,4 @@
-import { describe, test, beforeEach, afterEach } from "node:test";
-import * as vite from "vite";
-import path from "path";
+import { describe, test, before, after } from "node:test";
 import {
   Browser,
   openBrowser,
@@ -9,67 +7,81 @@ import {
   hmrTest,
   cleanHmrTest,
   friendlyClassNameTest,
+  findFreePort,
+  killPortProcess,
 } from "@internal/test-utils";
 import { CARD_COMPONENT_PATH, VITE_ROOT } from "./utils";
+import * as cp from "node:child_process";
 
 describe("dev", () => {
-  let devServer: vite.ViteDevServer;
-  let serverUrl: string;
+  let port: number;
+  let child: cp.ChildProcessWithoutNullStreams;
+  let previewUrl: string;
   let browser: Browser;
 
-  beforeEach(async () => {
-    devServer = await vite.createServer({
-      root: VITE_ROOT,
-      server: {
-        port: 0,
-        host: "127.0.0.1",
-      },
+  before(async () => {
+    port = await findFreePort();
+    previewUrl = `http://localhost:${port}/iframe.html?id=demo-card--default&viewMode=story`;
+
+    child = await new Promise<typeof child>((resolve, reject) => {
+      const child = cp.spawn(
+        "pnpm",
+        ["storybook", "--ci", "--port", port.toString()],
+        {
+          cwd: VITE_ROOT,
+        }
+      );
+
+      child.on("exit", (code) => {
+        if (code !== 0) {
+          reject(new Error("storybook exited with non-zero exit code"));
+        }
+      });
+
+      child.stdout.on("data", (data) => {
+        if (
+          data.toString().includes("Storybook") &&
+          data.toString().includes("started")
+        ) {
+          resolve(child);
+        }
+      });
     });
-
-    devServer = await devServer.listen();
-
-    const address = devServer.httpServer.address();
-
-    if (typeof address === "string") {
-      serverUrl = address;
-    } else {
-      serverUrl = `http://${address.address}:${address.port}`;
-    }
 
     browser = await openBrowser();
   });
 
-  afterEach(async () => {
-    await devServer.close();
+  after(async () => {
     await closeBrowser(browser);
+    await killPortProcess(port);
   });
 
   test("runtime injection works", async () => {
     const page = await browser.newPage();
-    await page.goto(serverUrl);
+    await page.goto(previewUrl);
 
     await runtimeInjectionTest(page);
   });
 
   test("friendly classnames work", async () => {
     const page = await browser.newPage();
-    await page.goto(serverUrl);
+    await page.goto(previewUrl);
 
     await friendlyClassNameTest(page);
   });
 
   describe("hmr", () => {
-    beforeEach(async () => {
+    before(async () => {
       await cleanHmrTest(CARD_COMPONENT_PATH);
     });
 
-    afterEach(async () => {
+    after(async () => {
       await cleanHmrTest(CARD_COMPONENT_PATH);
     });
 
     test("updating a style works", async () => {
       const page = await browser.newPage();
-      await page.goto(serverUrl);
+      await page.goto(previewUrl);
 
       await hmrTest(page, CARD_COMPONENT_PATH);
     });
