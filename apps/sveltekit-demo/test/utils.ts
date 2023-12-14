68170dc6fb7351c98dfa4c23aac428d25197c266
Horus Lugo <horusgoul@gmail.com>
2023-12-14 02:41:18 +0100

    e2e tests for SvelteKit

---
+++ apps/sveltekit-demo/test/utils.ts
@@ -0,0 +1,8 @@
+import path from "path";
+
+export const __dirname = path.dirname(new URL(import.meta.url).pathname);
+export const CARD_COMPONENT_PATH = path.resolve(
+  __dirname,
+  "../src/Card.svelte"
+);
+export const VITE_ROOT = path.resolve(__dirname, "../");
