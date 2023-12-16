620953a7d5e2372323853dd9027248b73760385f
Horus Lugo <horusgoul@gmail.com>
2023-12-16 18:22:12 +0100

    Build tests

--- apps/qwik-demo/test/utils.ts
+++ apps/qwik-demo/test/utils.ts
@@ -3,4 +3,3 @@ import path from "path";
 export const __dirname = path.dirname(new URL(import.meta.url).pathname);
 export const CARD_COMPONENT_PATH = path.resolve(__dirname, "../src/Card.tsx");
 export const VITE_ROOT = path.resolve(__dirname, "../");
-export const E2E_TEMP_DIR = path.resolve(__dirname, "../.e2e-temp");
