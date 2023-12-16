03cf978710d9910a8e75901b29afb03ed852ab16
Horus Lugo <horusgoul@gmail.com>
2023-12-16 18:02:17 +0100

    Qwik dev tests

--- apps/qwik-demo/vite.config.ts
+++ apps/qwik-demo/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { UserConfig, UserConfigExport, defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
