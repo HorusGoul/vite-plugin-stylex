cf9189b681a85824887d5f923753b399532c56c9
Horus Lugo <horusgoul@gmail.com>
2023-12-22 04:23:33 +0100

    Setup demo

--- apps/vite-demo/src/Card.tsx
+++ apps/vite-demo/src/Card.tsx
@@ -1,5 +1,6 @@
 import * as stylex from "@stylexjs/stylex";
 import { tokens } from "./theme.stylex";
+import { grayOKLCH } from "@stylexjs/open-props/lib/grayOKLCH.stylex";
 
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
@@ -16,5 +17,8 @@ const styles = stylex.create({
     padding: 16,
     boxShadow: "0 0 16px rgba(0, 0, 0, 0.1)",
     color: tokens.primaryTextColor,
+    ":hover": {
+      backgroundColor: `oklch(${grayOKLCH.grey1} 0 0)`,
+    },
   },
 });
