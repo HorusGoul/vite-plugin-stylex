069756f9fa25bb88ad86169dab14c85f994f233e
Horus Lugo <horusgoul@gmail.com>
2024-03-29 15:04:18 +0100

    Fixes building projects that include assets and use LightningCSS

--- apps/remix-demo/app/Card.tsx
+++ apps/remix-demo/app/Card.tsx
@@ -4,9 +4,12 @@ import { colors } from "@stylexjs/open-props/lib/colors.stylex";
 
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div {...stylex.props(styles.root)} data-testid="card">
-      {children}
-    </div>
+    <>
+      <div {...stylex.props(styles.root)} data-testid="card">
+        {children}
+      </div>
+      <img {...stylex.props(styles.asset)} />
+    </>
   );
 }
 
@@ -18,5 +21,10 @@ const styles = stylex.create({
     boxShadow: "0 0 16px rgba(0, 0, 0, 0.1)",
     color: tokens.primaryTextColor,
     border: `1px solid ${colors.blue1}`,
+  },
+  asset: {
+    backgroundImage: "url(/vite.svg)",
+    width: 32,
+    height: 32,
   },
 });
